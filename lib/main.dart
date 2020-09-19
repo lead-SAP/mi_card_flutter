ed23092a42f2783faa36891e2fdac039537ee8ff
عمرو عبدالله <captainamrabdullah@gmail.com>
2020-09-19 18:19:15 +0300

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/danish.jpg'),
+              ),
+              Text(
+                'Danish',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
