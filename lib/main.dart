4ca9377a1d0974fcfd574520618440b0e0caae53
عمرو عبدالله <captainamrabdullah@gmail.com>
2020-09-26 19:02:56 +0300

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,77 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
-                backgroundImage: AssetImage('images/danish.jpg'),
+                backgroundImage: AssetImage('images/passport.jpg'),
               ),
               Text(
-                'Danish',
+                'Amr Abdullah',
                 style: TextStyle(
                   fontSize: 40.0,
+                  fontFamily: 'Satisfy',
                   color: Colors.white,
                   fontWeight: FontWeight.bold
                 ),
+              ),
+              Text(
+                'Mobile Developer',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.white,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.normal
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,
+                horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+90 533 533 5333',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )
+              ),
+              Card(
+                  color: Colors.white, //by default, card comes with white color..so we can delete this
+                  margin: EdgeInsets.symmetric(vertical: 10.0,
+                      horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'amrabdullah@mobileDev.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18,
+                      ),
+                    ),
+
+
+                  )
               )
             ],
           )
@@ -34,9 +93,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-
